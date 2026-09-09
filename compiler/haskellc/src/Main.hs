26155bc25a60c89299e6842768ee61274d8d0c68
oleg.icefield <oleg.icefield@gmail.com>
2026-09-09 13:48:19 +0300

    haskellc: read module #3

--- compiler/haskellc/src/Main.hs
+++ compiler/haskellc/src/Main.hs
@@ -1,8 +1,18 @@
 module Main where
 
 import CompilerOptionsParser (CompilerOptions (..), parseCompilerOptions)
+import HaskellLexer (readModule)
+
+processModule :: String -> CompilerOptions -> IO ()
+processModule moduleName _options = do
+  result <- readModule moduleName
+  case result of
+    Left err -> putStrLn $ "Failed process module \"" ++ moduleName ++ "\" with errors: " ++ show err
+    Right content -> putStrLn $ "Success on process module \"" ++ moduleName ++ "\"\n  - Content:\n" ++ content
 
 main :: IO ()
 main = do
   options <- parseCompilerOptions
   putStrLn $ "Main module: " ++ mainModule options
+  processModule (mainModule options) options
+  putStrLn $ "End!"
