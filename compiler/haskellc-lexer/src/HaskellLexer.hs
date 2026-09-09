ac63f266953187b63bf82bd41958d6677abdb7e5
oleg.icefield <oleg.icefield@gmail.com>
2026-09-09 13:47:57 +0300

    haskellc-lexer: read module #3

--- compiler/haskellc-lexer/src/HaskellLexer.hs
+++ compiler/haskellc-lexer/src/HaskellLexer.hs
@@ -1,1 +1,6 @@
-module HaskellLexer () where
+module HaskellLexer (readModule) where
+
+import Control.Exception (IOException, try)
+
+readModule :: String -> IO (Either IOException String)
+readModule moduleName = try (readFile moduleName)
