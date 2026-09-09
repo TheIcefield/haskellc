7b4fc71d55aff5e3a95e40d83703a747eb7739b1
oleg.icefield <oleg.icefield@gmail.com>
2026-09-09 13:52:44 +0300

    haskellc-lexer: find source by module name #3

--- compiler/haskellc-lexer/src/HaskellLexer.hs
+++ compiler/haskellc-lexer/src/HaskellLexer.hs
@@ -2,5 +2,8 @@ module HaskellLexer (readModule) where
 
 import Control.Exception (IOException, try)
 
+pathFromModuleName :: String -> String
+pathFromModuleName moduleName = moduleName ++ ".hs"
+
 readModule :: String -> IO (Either IOException String)
-readModule moduleName = try (readFile moduleName)
+readModule moduleName = try (readFile (pathFromModuleName moduleName))
